627ef189edf460390d0b72b3c1db1d2b5b598c2e
tieupham2803 <tieupham2803@gmail.com>
2018-07-18 17:37:51 +0700

    fix convention

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -12,7 +12,8 @@ try {
     window.$ = window.jQuery = require('jquery');
 
     require('bootstrap');
-} catch (e) {}
+} catch (e) {
+}
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -54,3 +55,4 @@ if (token) {
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     encrypted: true
 // });
+
